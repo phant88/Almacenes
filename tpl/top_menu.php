cd8dc5d7f7bd6218ed4eee3102150d788e97efb2
Lucas Coaquira <l.coaquira@gmail.com>
2015-02-01 06:59:25 -0500

    Se agrego un include del menu del top

---
+++ tpl/top_menu.php
@@ -0,0 +1,10 @@
+<ul id="tabs" class="fl">
+					<li><a href="dashboard.php" class="dashboard-tab">Panel de Control</a></li>
+					<li><a href="view_sales.php" class="sales-tab">Ventas</a></li>
+					<li><a href="view_customers.php" class=" customers-tab">Clientes</a></li>
+					<li><a href="view_purchase.php" class="active-tab purchase-tab">Compras</a></li>
+					<li><a href="view_supplier.php" class=" supplier-tab">Proveedores</a></li>
+					<li><a href="view_product.php" class=" stock-tab">Stock / Productos</a></li>
+					<li><a href="view_payments.php" class="payment-tab">Pagos / Importe Total</a></li>
+					<li><a href="view_report.php" class="report-tab">Reportes</a></li>
+</ul> <!-- end tabs -->		
